3dcbdc514b432ee44a19d622a84f2bd39470e914
Robert <rlangamon2@csus.edu>
2023-09-24 23:44:28 -0700

    Modified layout again.

--- src/Demo.js
+++ src/Demo.js
@@ -44,25 +44,27 @@ export const Demo = () => {
     // <button onClick={() => {addDevice()}}>Add Device</button>
     return (
         <div className="home-container">
-        <header data-role="Header" className="home-header">
-            <h1 className="home-text">Cyber Savants Test Tracker</h1>
-            <div className="home-nav">
-            <nav className="home-nav1">
-                <span className="home-home-text">Home</span>
-                <span className="home-org-text">Organization</span>
-                <span className="home-text01">Archived Devices</span>
-                <span className="home-text02">Other</span>
-            </nav>
+        <header data-role="Header" className="home-top-header">
+            <h1 className="home-top-header-title">Cyber Savants Test Tracker</h1>
+            <div className="home-top-header-nav">
+            <span className="home-home-text">Home</span>
+            <span className="home-org-text">Organization</span>
+            <span className="home-arch-dev-text">Archived Devices</span>
+            <span className="home-other-text">Other</span>
             </div>
-            <input type="text" placeholder="Search..." className="input" />
-            <div className="home-btn-group">
-            <button className="home-login button">Login</button>
-            <button className="home-login button">Register</button>
+            <input
+            type="text"
+            placeholder="Search..."
+            className="home-search-bar input"
+            />
+            <div className="home-top-header-login">
+            <button className="home-login-button button">Login</button>
+            <button className="home-register-button button">Register</button>
             </div>
         </header>
-        <div className="home-features">
-            <div className="home-container1">
-            <h1 className="home-text03">Home - Test Devices</h1>
+        <div className="home-home-test-devices">
+            <div className="home-test-devices-header">
+            <h1 className="home-test-devices-text">Home - Test Devices</h1>
             <button type="button" className="home-add-device-button button">
                 <span>
                 <span>Add device</span>
@@ -70,87 +72,50 @@ export const Demo = () => {
                 </span>
             </button>
             </div>
-            <div className="home-container2">
+            <div className="home-test-devices-container">
             <div className="home-device1">
-                <h2 className="home-text07">
-                <span>Device 1</span>
+                <h2 className="home-device1title">
+                <span>Device #</span>
                 <br></br>
                 </h2>
-                <span className="home-text10">Status: Completed</span>
-                <button type="button" className="button">
+                <span className="home-device1status">Status: Completed</span>
+                <button type="button" className="home-device1button button">
                 View tests
                 </button>
             </div>
-            <div className="home-device2">
-                <h2 className="home-text11">
-                <span>Device 2</span>
+            <div className="home-device1">
+                <h2 className="home-device1title">
+                <span>Device #</span>
                 <br></br>
                 </h2>
-                <span className="home-text14">Status: In Progress</span>
-                <button type="button" className="button">
+                <span className="home-device1status">Status: In Progress</span>
+                <button type="button" className="home-device1button button">
                 View tests
                 </button>
             </div>
-            <div className="home-device3">
-                <h2 className="home-text15">
-                <span>Device 3</span>
+            <div className="home-device1">
+                <h2 className="home-device1title">
+                <span>Device #</span>
                 <br></br>
                 </h2>
-                <span className="home-text18">Status: In Progress</span>
-                <button type="button" className="button">
+                <span className="home-device1status">Status: In Progress</span>
+                <button type="button" className="home-device1button button">
                 View tests
                 </button>
             </div>
-            <div className="home-device4">
-                <h2 className="home-text19">
-                <span>Device 4</span>
+            <div className="home-device1">
+                <h2 className="home-device1title">
+                <span>Device #</span>
                 <br></br>
                 </h2>
-                <span className="home-text22">Status: Not Started</span>
-                <button type="button" className="button">
+                <span className="home-device1status">Status: Not Started</span>
+                <button type="button" className="home-device1button button">
                 View tests
                 </button>
             </div>
             </div>
         </div>
         </div>
-
-        /*
-        <div className="App">
-            <div className="title">Cyber Savants Device Test Tracker</div>
-            <div className="login">Welcome, Cyber Savants.</div>
-            <div className="box-container">
-                <div className="box">
-                    <h2>Device 1</h2>
-                    <p>Status: Completed</p>
-                    <button>View tests</button>
-                </div>
-                <div className="box">
-                    <h2>Device 2</h2>
-                    <p>Status: In Progress</p>
-                    <button>View tests</button>
-                </div>
-                <div className="box">
-                    <h2>Device 3</h2>
-                    <p>Status: In Progress</p>
-                    <button>View tests</button>
-                </div>
-                <div className="box">
-                    <h2>Device 4</h2>
-                    <p>Status: Not Started</p>
-                    <button>View tests</button>
-                </div>
-            </div>
-            <div className="centered-button">
-                <button className="large-button">Add device</button>
-            </div>
-            <div className="bottom-right-button">
-                <button className="large-button">View archived devices</button>
-            </div>
-        </div>
-        */
-
-
 
         /*
         </div>
