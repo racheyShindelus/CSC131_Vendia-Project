20b2a2a622d68d7395db2610efc14da5e4034a9c
jrata <jericrata@gmail.com>
2023-10-15 20:51:01 -0700

    Fixed redirecting to login on refresh

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,19 +1,18 @@
 import { Link } from 'react-router-dom';
 import { AuthDetails } from './components/auth/AuthDetails'
-import { useAuth } from './AuthContext';
-
+import {useData} from './DataContext';
 const Navbar = () => {
-    const { authUser } = useAuth();
-  
+    const {userData} = useData();
     return (
-      <header className="sticky top-0 w-full flex overflow-auto items-center flex-col justify-start ">
+      <header className="z-10 sticky top-0 w-full flex overflow-auto items-center flex-col justify-start ">
         <div className="overflow-auto w-full flex items-center pt-8 pb-4 border-b border-black bg-white px-8 justify-between">
           <div className="flex relative space-x-8">
             <h1 className="text-xl font-bold">Cyber Savants Test Tracker</h1>
-            <Link to="/Home" className="text-lg text-black">Home</Link>
-            <Link to="/Archive" className="text-lg text-black">Archived Devices</Link>
-            <Link to="/DevicePage" className="text-lg text-black">Organization</Link>
-            <Link to="/Demo" className="text-lg text-black">Other</Link>
+            <Link to="/Home" className="text-lg text-black hover:scale-105 hover:text-blue-800">Home</Link>
+            <Link to="/Archive" className="text-lg text-black hover:scale-105 hover:text-blue-800">Archived Devices</Link>
+            <Link to="/DevicePage" className="text-lg text-black hover:scale-105 hover:text-blue-800">Organization</Link>
+            <Link to="/Demo" className="text-lg text-black hover:scale-105 hover:text-blue-800">Other</Link>
+            {(userData?.role === 'owner' || userData?.role === 'admin') && <Link to ='/AdminPage' className ='"text-lg text-black hover:scale-105 hover:text-blue-800'>Admin</Link>}
           </div>
           <input
             type="text"
