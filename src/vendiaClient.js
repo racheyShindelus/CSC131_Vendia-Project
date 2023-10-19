3144c391ae354502307178bf2963c3799a332082
drewizzo <145307203+drewizzo@users.noreply.github.com>
2023-10-19 11:57:08 -0700

    updated API key

--- src/vendiaClient.js
+++ src/vendiaClient.js
@@ -2,9 +2,10 @@ import { createVendiaClient } from "@vendia/client";
 
 const client = createVendiaClient({
     apiUrl: `https://zc3kfaa6p7.execute-api.us-west-2.amazonaws.com/graphql/`,
-    apiKey: 'DcDYNMrdJM3Sib4eBHN4FZbUHXD8VVWu6Dtaa5iktNL4', // <---- API key
-})
+    apiKey: '7Wgz8nttNdnQKagjsRqXJVb96fokgfSf7kMdoA44U6Nw', // <---- API key
+  })
 
-export const vendiaClient = () => {
-    return {client};
-};
+export const vendiaClient = () =>
+{
+    return{client}
+}
