a0a0e42925194b6cc716eca9b6bf503c6680acb2
drewizzo <145307203+drewizzo@users.noreply.github.com>
2023-10-19 11:55:29 -0700

    Archive.js
    updated to import device entity, filters out non archived devices

--- src/Archive.js
+++ src/Archive.js
@@ -1,46 +1,57 @@
-import React, { useState } from "react";
+import React, {useEffect, useState} from "react";
+import { vendiaClient } from "./vendiaClient";
 import "./Archive.css";
 import "./App.css";
-import data from "./mock-data.json";
 
-const Archive = () => {
+const { client } = vendiaClient();
+
+export const Archive = () => {
     
-    const [Archives, setArchives] = useState(data);
-    const handleDeleteClick = (deviceID) => {
-        const newArchives = [...Archives];
-        const index = Archives.findIndex((Archive) => Archive.DeviceID === deviceID);
-        newArchives.splice(index, 1);
-        setArchives(newArchives);
-    };
+    const [Archives, setArchives] = useState([]);
+
+    useEffect(() => {
+        const archivedDevices = async () => {
+            const archiveList = await client.entities.devices.list({
+                filter: {
+                    Archived: {
+                        contains: 'true',
+                    }
+                },
+            })
+            
+            setArchives(archiveList?.items);
+        }
+        archivedDevices();
+    }, [])
+
+    const boolToString = (value) => {
+        if(typeof(value) === 'boolean')
+            return 'true';
+        else   
+            return 'false';
+    }
+    
+  
     
     return ( 
-    <div className="home-container">
+    <div className = "home-container">   
     <div className = "archive">
         <table>
-            <caption>All Archived Devices</caption>
             <thead>
                 <tr>
-                    <th>Device ID</th>
                     <th>Device Name</th>
-                    <th>OrgAssignment</th>
-                    <th>Tester</th>
-                    <th>Remove Archive</th>
+                    <th>Completion</th>
+                    <th>Archived</th>
+                    <th>Device Title</th>
                 </tr>
             </thead>
             <tbody>
-                {Archives.map((Archive) => (                
-                <tr key={Archive.DeviceID}>
-                    <td>{Archive.DeviceID}</td>
-                    <td>{Archive.DeviceName}</td>
-                    <td>{Archive.OrgAssignment}</td>
-                    <td>{Archive.Tester}</td>
-                    <td>
-                        <button
-                        type = "button"
-                        onClick = {() => handleDeleteClick(Archive.DeviceID)}>
-                        </button>
-                        Delete
-                    </td>
+                {Archives?.map((device) => (                
+                <tr>
+                    <td>{device?.DeviceName}</td>
+                    <td>{device?.Completion}</td>
+                    <td>{device?.Archived}</td>
+                    <td>{boolToString(device?.DeviceTitle)}</td>
                 </tr>
                 ))}
 
