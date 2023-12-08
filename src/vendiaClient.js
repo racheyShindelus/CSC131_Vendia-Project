e2ca08aba26784e95fbcb6be77b617fd81ae7970
jrata <jericrata@gmail.com>
2023-12-07 20:40:50 -0800

    Add Device/Test Data Validation, Added Progress Bar, Changed Datagrid styling, Added User Feedback

--- src/vendiaClient.js
+++ src/vendiaClient.js
@@ -1,35 +1,35 @@
-import { createVendiaClient } from "@vendia/client";
+// import { createVendiaClient } from "@vendia/client";
 
-const client = createVendiaClient({
-    apiUrl: `https://zc3kfaa6p7.execute-api.us-west-2.amazonaws.com/graphql/`,
-    apiKey: '6UfzXiK4sUKW7rdhG4fD3WZLM8yrc6KRNVeV9CipceKU', // <---- API key
-    websocketUrl: 'wss://vbgugj44u0.execute-api.us-west-2.amazonaws.com/graphql',
-})
+// const client = createVendiaClient({
+//     apiUrl: `https://zc3kfaa6p7.execute-api.us-west-2.amazonaws.com/graphql/`,
+//     apiKey: '7Wgz8nttNdnQKagjsRqXJVb96fokgfSf7kMdoA44U6Nw', // <---- API key
+//   })
 
-export const vendiaClient = () => {
-    return {client};
-};
+// export const vendiaClient = () =>
+// {
+//     return{client}
+// }
 
-export const VendiaClient = {
-    uploadTestData: async (testData) => {
-      try {
-        const response = await client.mutate({
-        });
-        return response.data;
-      } catch (error) {
-        console.error('Error uploading test data to Vendia: ', error);
-        throw error;
-      }
-    },
-  
-    uploadDeviceData: async (deviceData) => {
-      try {
-        const response = await client.mutate({
-        });
-        return response.data;
-      } catch (error) {
-        console.error('Error uploading device data to Vendia: ', error);
-        throw error;
-      }
-    },
-  };
+// export const VendiaClient = {
+//   uploadTestData: async (testData) => {
+//     try {
+//       const response = await client.mutate({
+//       });
+//       return response.data;
+//     } catch (error) {
+//       console.error('Error uploading test data to Vendia: ', error);
+//       throw error;
+//     }
+//   },
+
+//   uploadDeviceData: async (deviceData) => {
+//     try {
+//       const response = await client.mutate({
+//       });
+//       return response.data;
+//     } catch (error) {
+//       console.error('Error uploading device data to Vendia: ', error);
+//       throw error;
+//     }
+//   },
+// };
