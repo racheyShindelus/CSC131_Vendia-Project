d2d23f74f61a0bdc6081c83feff87cd986925826
Robert <rlangamon2@csus.edu>
2023-12-09 10:56:54 -0800

    Added Jeric's changes, prepared website for demo.

--- src/ArchiveList.js
+++ src/ArchiveList.js
@@ -4,7 +4,7 @@ import { vendiaClient } from "./vendiaClient";
 import "./Archive.css";
 import './App.css'
 import { Link } from 'react-router-dom';
-import Pagination from '@mui/material/Pagination';
+import {Pagination, Grid, LinearProgress, Typography} from '@mui/material';
 
 const { client } = vendiaClient();
 
@@ -38,17 +38,35 @@ const ArchiveList = (deviceProps) => {
             <div className="items-start w-auto pb-5 h-auto grid gap-y-[30px] grid-cols-2 lg:grid-cols-4 lg:grid-rows-3 md:grid-cols-2 md:grid-rows-6">
                 {currentItems?.map((item, index) => (
                 <div className="shadow-custom w-[90%] flex p-[16px] border border-gray-300 max-w-[MaxWidth] transition-transform transition-shadow transition duration-300 items-start flex-col justify-start bg-white hover:scale-[1.02] hover:shadow-indigo-300" key={index}>
-                    <h2 className="mb-[5px] text-[20px] mt-0 font-bold">
-						#{(page - 1) * itemsPerPage + index + 1}: {item?.DeviceTitle}
-					</h2>
-                    <p className="text-[16px] mb-[18px]">
-						Status: Archived
-					</p>
-                    <Link to={`/ArchiveTests/${item?.DeviceName}/${item?.DeviceTitle}`}
-					className="min-w-[40%] h-[35px] flex text-inherit text-[10px] sm:text-[10px] md:text-[12px] lg:text-[16px] xl:text-[16px] items-center justify-center rounded-[10px] bg-gray-200 border border-black no-underline hover:bg-gray-300"
-                    type="button">
-                        View tests
-                    </Link>
+                    <Grid
+                            container
+                            spacing={1}
+                            justifyContent="center"
+                            direction="column"
+                    >
+						<Grid item xs={12} sm={6} md={4} lg={3}>
+							<h2 className="mb-[5px] text-[20px] mt-0 font-bold">
+								#{(page - 1) * itemsPerPage + index + 1}: {item?.DeviceTitle}
+							</h2>
+						</Grid>
+						<Grid item xs={12} sm={6} md={4} lg={3} className="relative">
+							<LinearProgress
+									variant="determinate"
+									value={parseInt(item?.Completion, 10)}
+									sx={{ height: 25, borderRadius: 5 }}
+									/>
+										<Typography  style={{left: `50%`,
+															color: "white",
+															}} variant="body2" color="Primary" className="absolute top-5 left-1/2 transform -translate-x-1/2 -translate-y-1/2">{`Archived`}</Typography>
+						</Grid>
+						<Grid item xs={12} sm={6} md={4} lg={3}>
+							<Link to={`/ArchiveTests/${item?.DeviceName}/${item?.DeviceTitle}`}
+							className="min-w-[40%] h-[35px] flex text-inherit text-[10px] sm:text-[10px] md:text-[12px] lg:text-[16px] xl:text-[16px] items-center justify-center rounded-[10px] bg-gray-200 border border-black no-underline hover:bg-gray-300"
+							type="button">
+								View tests
+							</Link>
+						</Grid>
+					</Grid>
                 </div>
                 ))}
             </div>
--- src/components/orgs/DisplayOrgUsers.jsx
+++ src/components/orgs/DisplayOrgUsers.jsx
@@ -2,27 +2,27 @@ import { useEffect, useState } from "react"
 import { db } from "../../firebase"
 import { onSnapshot, where, collection, getDoc, query, getDocs, doc, updateDoc } from 'firebase/firestore'
 import { DataGrid, GridToolbarQuickFilter } from '@mui/x-data-grid';
-import Dialog from '@mui/material/Dialog';
-import DialogTitle from '@mui/material/DialogTitle';
-import DialogContent from '@mui/material/DialogContent';
-import DialogActions from '@mui/material/DialogActions';
-import Autocomplete from '@mui/material/Autocomplete';
-import Button from '@mui/material/Button';
-import { TextField } from "@mui/material";
+import { TextField, Avatar, Dialog, DialogTitle, DialogContent, DialogActions, Autocomplete, Button } from "@mui/material";
 import { useData } from "../../DataContext";
 import { vendiaClient } from "../../vendiaClient";
 import Checkbox from '@mui/material/Checkbox';
+import FeedbackMessage from "../generic/FeedbackMessage";
 
 const {client} = vendiaClient();
-
 export const DisplayOrgUsers = ({org}) => {
+    const adminPfp = 'https://upload.wikimedia.org/wikipedia/commons/d/dd/Eo_circle_green_white_letter-a.svg'
+    const userPfp = 'https://upload.wikimedia.org/wikipedia/commons/0/07/Eo_circle_pink_white_letter-u.svg'
     const [selectedUser, setSelectedUser] = useState([]);
     const {userData} = useData();
     const [otherUsers, setOtherUsers] = useState([]);
     const [rows, setRows] = useState([]);
     const [rowSelection, setRowSelection] = useState([]);
     const [manageUsersDialog, setManageUsersDialog] = useState(false);
-
+    const [feedback, setFeedback] = useState({
+      open: false,
+      message: "",
+      severity: "",
+    });
     useEffect(() => {
       const listOrgUsers = async () => {
         const usersCollection = collection(db, "users");
@@ -67,8 +67,9 @@ export const DisplayOrgUsers = ({org}) => {
         getDoc(userDocRef)
           .then((doc) => {
             if (doc.exists()) {
-              const data = doc.data();
-              const newOrgs = data.orgs;
+              const data = doc?.data();
+              const newOrgs = data?.orgs;
+              setFeedback({open: true, message:`${data.displayName} has been removed`, severity: 'error'})
               const deleteIndex = newOrgs.indexOf(org.OrgName);
               if (deleteIndex !== -1) {
                 newOrgs.splice(deleteIndex, 1);
@@ -81,110 +82,134 @@ export const DisplayOrgUsers = ({org}) => {
           })
           .catch((error) => {
             console.error('Error removing value:', error);
+            setFeedback({open: true, message: error.message, severity: 'error'})
           });
       }
     }
-
     const handleAddUsers = async () => {
-      selectedUser.forEach(async(user) => {
-        const userDocRef = await doc(db, 'users', user.id)
-          getDoc(userDocRef)
-            .then((doc) => {
-              if(doc.exists()) {
-                const data = doc.data();
-                const newOrgs = data.orgs || [];
-              if(!newOrgs.includes(org?.OrgName)){
-                  newOrgs.push(org.OrgName)
-                  return updateDoc(userDocRef, {orgs: newOrgs})
+        selectedUser.forEach(async(user) => {
+          const userDocRef = await doc(db, 'users', user.id)
+            getDoc(userDocRef)
+              .then((doc) => {
+                if(doc.exists()) {
+                  const data = doc.data();
+                  const newOrgs = data.orgs || [];
+                if(!newOrgs.includes(org?.OrgName)){
+                    newOrgs.push(org.OrgName)
+                    setFeedback({open: true, message:`${data.displayName} has been added`, severity: 'success'})
+                    return updateDoc(userDocRef, {orgs: newOrgs})
+                  }
                 }
-              }
-            })
-            .then(() => {
-              console.log(`Value '${org?.OrgName}' added from the 'orgs' array.`);
-            })
-            .catch((error)=> {
-              console.error('Error adding value:', error)
-            })
-      })
-      setSelectedUser([]);
-      setManageUsersDialog(false)
+              })
+              .then(() => {
+                console.log(`Value '${org?.OrgName}' added to the 'orgs' array.`);
+              })
+              .catch((error)=> {
+                console.error('Error adding value:', error)
+                setFeedback({open: true, message: error.message, severity: 'error'})
+              })
+        })
+        setSelectedUser([]);
+        setManageUsersDialog(false)
     }
-
     const columns = [
-      { field: 'displayName', headerName: "Username", flex:2 },
-      { field: 'isAdmin', headerName: 'Admin', flex:2 },
-      { field: 'email', headerName: 'Email', flex:2 },
-    ]
-    
-    return(
-      <div>
-          <div className="flex space-x-4">
-              {userData.isAdmin && (
-                <button
-                  onClick={() => setManageUsersDialog(true)}
-                  className="my-2 w-28 h-8 text-base flex items-center justify-center font-bold no-underline rounded-2xl bg-green-600 text-white shadow-md hover:bg-green-700"
-                >
-                  Assign Users
-                </button>
-              )}
-              {userData.isAdmin && (
-                <button
-                  onClick={deleteRow}
-                  className="my-2 w-36 h-8 text-base flex items-center justify-center font-bold no-underline rounded-2xl bg-red-600 text-white shadow-md hover:bg-red-700"
-                >
-                  Unassign Users
-                </button>
-              )}
-            </div> 
-            <DataGrid
-            rows={rows}
-            columns={columns}
-            pageSize={5}
-            getRowId={(row) => row?.ID}
-            checkboxSelection
-            rowSelectionModel={rowSelection}
-            onRowSelectionModelChange={(newRowSelection) => {
-              setRowSelection(newRowSelection);
-            }}
-            slots={{ toolbar: GridToolbarQuickFilter }}
-            />
-          <Dialog open={manageUsersDialog} onClose={() => setManageUsersDialog(false)} >
-              <DialogTitle>Assign Users</DialogTitle>
-                <DialogContent className="items-center w-80 md:w-128">
-
-                <Autocomplete
-                    className="mt-4"
-                    id="user-select"
-                    options={otherUsers}
-                    getOptionLabel={(option) => option.displayName}
-                    value={selectedUser}
-                    onChange={(event, newValue) => setSelectedUser(newValue)}
-                    multiple
-                    disableCloseOnSelect
-                    renderInput={(params) =><TextField {...params} label="Users" />}
-                    renderOption={(props, option) => (
-                      <li {...props} className="flex justify-between items-center p-2 border-b">
-                        <div>
-                          <div className="font-bold">{option.displayName}<span className="font-normal text-gray-400"> {option.orgs.join(', ')}</span></div>
-                          <div className="text-sm">{option.email}</div>
-                        </div>
-                        <Checkbox
-                          color="primary"
-                          checked={selectedUser.some((user) => user.id === option.id)}
-                        />
-                      </li>
-                    )}
-                  />
-                </DialogContent>
-              <DialogActions>
-                <Button onClick={handleAddUsers} color="primary">
-                    Assign
+      {field: 'avatar', headerName: 'Avatar', width: 80,
+        renderCell: (params) => (
+          <div className="flex mx-auto">
+            {params.row.isAdmin ? (
+              <Avatar src={adminPfp} alt="admin avatar" />
+            ) : (
+              <Avatar src={userPfp} alt="user avatar" />
+            )}
+          </div>
+        ),
+      },
+        { field: 'displayName', headerName: "Username", flex:2 },
+        { field: 'isAdmin', headerName: 'Status', flex:2, renderCell: (params) => (
+          <div>{params.value ? 'Admin' : 'User'}</div>
+        ),},
+        { field: 'email', headerName: 'Email', flex:2 },
+      ]
+      return(
+        <div>
+            <div className="flex space-x-4">
+                {userData.isAdmin && (
+                  <button
+                    onClick={() => setManageUsersDialog(true)}
+                    className="my-2 w-28 h-8 text-base flex items-center justify-center font-bold no-underline rounded-2xl bg-green-600 text-white shadow-md hover:bg-green-700"
+                  >
+                    Assign Users
+                  </button>
+                )}
+                {userData.isAdmin && (
+                  <button
+                    onClick={deleteRow}
+                    className="my-2 w-36 h-8 text-base flex items-center justify-center font-bold no-underline rounded-2xl bg-red-600 text-white shadow-md hover:bg-red-700"
+                  >
+                    Unassign Users
+                  </button>
+                )}
+              </div> 
+              <DataGrid
+              rows={rows}
+              columns={columns}
+              pageSize={5}
+              getRowId={(row) => row?.ID}
+              checkboxSelection
+              rowSelectionModel={rowSelection}
+              onRowSelectionModelChange={(newRowSelection) => {
+                setRowSelection(newRowSelection);
+              }}
+              slots={{ toolbar: GridToolbarQuickFilter }}
+              />
+            <FeedbackMessage
+              open={feedback.open}
+              message={feedback.message}
+              severity={feedback.severity}
+              handleClose={() => setFeedback({ open: false, message: feedback.message, severity: feedback.severity })}
+              />
+            <Dialog open={manageUsersDialog} onClose={() => setManageUsersDialog(false)} >
+                <DialogTitle>Assign Users</DialogTitle>
+                  <DialogContent className="items-center w-96 md:w-128">
+                  <Autocomplete
+                      className="mt-4 w-82 md:w-112"
+                      id="user-select"
+                      options={otherUsers}
+                      getOptionLabel={(option) => option.displayName}
+                      value={selectedUser}
+                      onChange={(event, newValue) => setSelectedUser(newValue)}
+                      multiple
+                      disableCloseOnSelect
+                      renderInput={(params) =><TextField {...params} label="Users" />}
+                      renderOption={(props, option) => (
+                        <li {...props} className="flex justify-between items-center p-2 border-b">
+                          <div className="flex items-center">
+                            <Avatar src={option.isAdmin ? adminPfp : userPfp} />
+                            <div className="ml-2">
+                              <div className="font-bold">
+                                <span>{option.displayName}</span>
+                                <span className="font-normal text-gray-400"> {option.orgs.join(', ')}</span>
+                              </div>
+                              <div className="text-sm">{option.email}</div>
+                            </div>
+                          </div>
+                          <Checkbox
+                            color="primary"
+                            checked={selectedUser.some((user) => user.id === option.id)}
+                          />
+                        </li>
+                      )}
+                    />
+                  </DialogContent>
+                <DialogActions>
+                  <Button onClick={handleAddUsers} color="primary">
+                      Assign
+                    </Button>
+                  <Button onClick={()=>setManageUsersDialog(false)} color="primary">
+                    Cancel
                   </Button>
-                <Button onClick={()=>setManageUsersDialog(false)} color="primary">
-                  Cancel
-                </Button>
-              </DialogActions>
-           </Dialog>
-      </div>
-    )
+                </DialogActions>
+             </Dialog>
+        </div>
+      )
 }
